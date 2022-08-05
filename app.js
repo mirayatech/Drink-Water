f18588760e73ce4b1620db87611a03070cdb3ab0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-05 06:30:01 +0200

    selecting required elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const smallVups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const precentages = document.getElementById("precentage");
+const remained = document.getElementById("remained");
