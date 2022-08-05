6ff28919fb4d7da1ef4e60ac0e2cd8ff869de7ac
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-05 06:42:47 +0200

    filling smal lcups

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const smallVups = document.querySelectorAll(".cup-small");
+const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const precentages = document.getElementById("precentage");
 const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener("click", () => highlightCups(index));
+});
+
+function highlightCups(index) {
+  if (
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("fill")
+  ) {
+    index--;
+  }
+
+  smallCups.forEach((cup, indexCup) => {
+    if (indexCup <= index) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
