5785c1957490019cfd11d73215def45db4ac4fd9
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-05 07:03:21 +0200

    full big cup

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
-const precentages = document.getElementById("precentage");
+const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
-smallCups.forEach((cup, index) => {
-  cup.addEventListener("click", () => highlightCups(index));
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
 });
 
-function highlightCups(index) {
-  if (
-    smallCups[index].classList.contains("full") &&
-    !smallCups[index].nextElementSibling.classList.contains("fill")
+function highlightCups(idx) {
+  if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
+  else if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
   ) {
-    index--;
+    idx--;
   }
 
-  smallCups.forEach((cup, indexCup) => {
-    if (indexCup <= index) {
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
       cup.classList.add("full");
     } else {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
